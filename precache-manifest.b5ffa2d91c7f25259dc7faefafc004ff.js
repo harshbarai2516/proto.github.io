bd114f477e2d5f513c741ec03fd4e0e6da4ce8f0
Harsh Barai <harshbarai52@gmail.com>
2020-12-12 11:16:58 +0530

    Updates

---
+++ precache-manifest.b5ffa2d91c7f25259dc7faefafc004ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "19e3da52d50c99a77342",
+    "url": "/johnwick3690/proto.github.io/static/css/main.a2a823c6.chunk.css"
+  },
+  {
+    "revision": "19e3da52d50c99a77342",
+    "url": "/johnwick3690/proto.github.io/static/js/main.e8bcf0b8.chunk.js"
+  },
+  {
+    "revision": "e3a9f74ae7d50cf1e0a0",
+    "url": "/johnwick3690/proto.github.io/static/js/runtime~main.0303bddb.js"
+  },
+  {
+    "revision": "eb04b6a2dcc6c1fae2cf",
+    "url": "/johnwick3690/proto.github.io/static/js/2.b4e1bc9a.chunk.js"
+  },
+  {
+    "revision": "49a3e1a6257c00b69b26",
+    "url": "/johnwick3690/proto.github.io/static/js/3.6e3511c1.chunk.js"
+  },
+  {
+    "revision": "c9ad6144030b6d45fd7864f0f8aa941c",
+    "url": "/johnwick3690/proto.github.io/index.html"
+  }
+];
